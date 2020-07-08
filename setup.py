7e9aa0523f3d46e09020f24f60b1b6e123c25681
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-07-08 18:04:28 -0400

    organize files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='blockwise',
+      version='0.1',
+      description='Fast blockwise operations of array data',
+      url='https://github.com/hungyiwu/blockwise',
+      author='Hung-Yi Wu',
+      author_email='hungyiwu@protonmail.com',
+      license='MIT', # TODO: which one to use?
+      packages=['blockwise'],
+      zip_safe=False)
