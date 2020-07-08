8a4427b43bbf1c851aa5c3a59fcced9a4a77d1fb
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-07-08 19:20:22 -0400

    editted according to official packaging guide

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-from setuptools import setup
+import setuptools
 
-setup(name='blockwise',
-      version='0.1',
-      description='Fast blockwise operations of array data',
-      url='https://github.com/hungyiwu/blockwise',
-      author='Hung-Yi Wu',
-      author_email='hungyiwu@protonmail.com',
-      license='MIT',
-      packages=['blockwise'],
-      zip_safe=False)
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='blockwise',
+    version='0.1',
+    author='Hung-Yi Wu',
+    author_email='hungyiwu@protonmail.com',
+    description='Fast blockwise operations of array data',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hungyiwu/blockwise',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    )
